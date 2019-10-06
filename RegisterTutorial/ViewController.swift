7fcd1dae9ed0abb3d6226e975f644b586dcff237
MateuSales <matheussalles40@gmail.com>
2019-10-06 19:50:59 -0300

    Add delete rows

--- RegisterTutorial/ViewController.swift
+++ RegisterTutorial/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     // MARK: Properties
     let tableViewRegister = TableViewRegister()
-    let mockResult: [MockCell] = [MockCell(image: #imageLiteral(resourceName: "zelda"), name: "Zelda"),MockCell(image: #imageLiteral(resourceName: "epona"), name: "Epona"),MockCell(image: #imageLiteral(resourceName: "link"), name: "Link")]
+    var mockResult: [MockCell] = [MockCell(image: #imageLiteral(resourceName: "zelda"), name: "Zelda"),MockCell(image: #imageLiteral(resourceName: "epona"), name: "Epona"),MockCell(image: #imageLiteral(resourceName: "link"), name: "Link")]
     
     // MARK: Inicialization
     override func viewDidLoad() {
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITabBarDelegate, UITableViewDataSource {
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mockResult.count
@@ -47,6 +47,15 @@ extension ViewController: UITabBarDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? TableViewCellRegister
         cell?.mockField = mockResult[indexPath.row]
         return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            print("Deleted")
+            
+            self.mockResult.remove(at: indexPath.row)
+            self.tableViewRegister.tableviewR.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
     
 }
