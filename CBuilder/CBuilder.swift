ee5363fbdb0d1c8587d5df9be6f32bbe34bbf6dc
Guilherme <guiaszul@gmail.com>
2019-10-07 00:15:44 -0300

    Adding CBuider class to simplify contraints in project

---
+++ CBuilder/CBuilder.swift
@@ -0,0 +1,142 @@
+//
+//  CBuilder.swift
+//  CBuilder
+//
+//  Created by Vinicius Mangueira on 28/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    
+    /// This method add arrange of view in view
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { (view) in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(view)
+        }
+    }
+    
+    func cBuilder(_ make : (LayoutProxy) -> Void) {
+        translatesAutoresizingMaskIntoConstraints = false
+        make(LayoutProxy(view: self))
+    }
+    
+    /// This constraints with you respective constant, always anchor following with you constant
+    func cBuild(top: NSLayoutYAxisAnchor?, costantTop: CGFloat = 0, bottom: NSLayoutYAxisAnchor?, constantBottom: CGFloat = 0, left: NSLayoutXAxisAnchor?, constantLeft: CGFloat = 0,  right: NSLayoutXAxisAnchor?, constantRight: CGFloat = 0) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let tope = top {
+            topAnchor.constraint(equalTo: tope, constant: costantTop).isActive = true
+        }
+        
+        if let bott = bottom {
+            bottomAnchor.constraint(equalTo: bott, constant: constantBottom).isActive = true
+        }
+        
+        if let leading = left {
+            leadingAnchor.constraint(equalTo: leading, constant: constantLeft).isActive = true
+        }
+        
+        if let trailing = right {
+            trailing.constraint(equalTo: trailing, constant: constantRight).isActive = true
+        }
+    }
+    
+    /// This method is calling to set all constraints in to respective anchors
+    func cBuild(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let tope = top {
+            topAnchor.constraint(equalTo: tope).isActive = true
+        }
+        
+        if let bott = bottom {
+            bottomAnchor.constraint(equalTo: bott).isActive = true
+        }
+        
+        if let leading = left {
+            leadingAnchor.constraint(equalTo: leading).isActive = true
+        }
+        
+        if let trailing = right {
+            trailing.constraint(equalTo: trailing).isActive = true
+        }
+    }
+    
+    /// Define width from anchor with constant
+    func cBuild(width: CGFloat) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    /// Define with from anchor with NSLayoutDimension
+    func cBuild(width: NSLayoutDimension) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalTo: width).isActive = true
+    }
+    
+    /// Define height from anchor with constant
+    func cBuild(height: CGFloat) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    /// Define height from anchor with NSLayoutDimension
+    func cBuild(height: NSLayoutDimension) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightAnchor.constraint(equalTo: height).isActive = true
+    }
+    
+    
+    /// Method calling when you to consider your view equal to
+    func cBuild(make: ViewAction) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        switch make {
+        case .fillSuperview:
+            equalToSuperView()
+        case .centerInSuperView:
+            equalToSuperView()
+        case .centerXInSuperView:
+            centerXInSuperView()
+        case .centerYInSuperView:
+            centerYInSuperView()
+        }
+    }
+    
+    fileprivate func centerInSuperView() {
+        centerXInSuperView()
+        centerYInSuperView()
+    }
+    
+    fileprivate func centerXInSuperView() {
+        guard let spView = superview else {return}
+        NSLayoutConstraint.activate([
+            centerXAnchor.constraint(equalTo: spView.centerXAnchor)
+            ])
+    }
+    
+    fileprivate func centerYInSuperView() {
+        guard let spView = superview else {return}
+        NSLayoutConstraint.activate([
+            centerYAnchor.constraint(equalTo: spView.centerYAnchor)
+            ])
+    }
+    
+    fileprivate func equalToSuperView() {
+        guard let spView = superview else {return}
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: spView.topAnchor),
+            leadingAnchor.constraint(equalTo: spView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: spView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: spView.bottomAnchor)
+            ])
+    }
+}
+
+enum ViewAction {
+    case fillSuperview
+    case centerInSuperView
+    case centerXInSuperView
+    case centerYInSuperView
+}
---
+++ CBuilder/LayoutAnchor.swift
@@ -0,0 +1,124 @@
+//
+//  LayoutAnchor.swift
+//  CBuilder
+//
+//  Created by Vinicius Mangueira on 28/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+protocol LayoutAnchor {
+    func constraint(equalTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(greaterThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(lessThanOrEqualTo anchor: Self, constant: CGFloat) -> NSLayoutConstraint
+}
+
+extension NSLayoutAnchor: LayoutAnchor {}
+
+protocol LayoutXAnchor {
+    func constraint(equalToSystemSpacingAfter anchor: NSLayoutXAxisAnchor, multiplier: CGFloat) -> NSLayoutConstraint
+    
+    func constraint(greaterThanOrEqualToSystemSpacingAfter anchor: NSLayoutXAxisAnchor, multiplier: CGFloat) -> NSLayoutConstraint
+    
+    func constraint(lessThanOrEqualToSystemSpacingAfter anchor: NSLayoutXAxisAnchor, multiplier: CGFloat) -> NSLayoutConstraint
+}
+
+
+extension NSLayoutXAxisAnchor: LayoutXAnchor {}
+
+struct LayoutProperty<Anchor: LayoutAnchor> {
+    fileprivate let anchor: Anchor
+}
+
+
+struct LayoutPropertyX<Anchor: LayoutXAnchor> {
+    fileprivate let anchor: Anchor
+}
+
+class LayoutProxy {
+    lazy var leading = property(with: view.leadingAnchor)
+    lazy var trailing = property(with: view.trailingAnchor)
+    lazy var top = property(with: view.topAnchor)
+    lazy var bottom = property(with: view.bottomAnchor)
+    lazy var width = property(with: view.widthAnchor)
+    lazy var height = property(with: view.heightAnchor)
+    
+    private let view: UIView
+    
+    init(view: UIView) {
+        self.view = view
+    }
+    
+    private func property<A: LayoutAnchor>(with anchor: A) -> LayoutProperty<A> {
+        return LayoutProperty(anchor: anchor)
+    }
+}
+
+extension LayoutPropertyX {
+    func equal(to otherAnchor: Anchor, multiplier: CGFloat = 0) {
+        if let anchor = otherAnchor as? NSLayoutXAxisAnchor {
+            anchor.constraint(equalToSystemSpacingAfter: anchor, multiplier: multiplier).isActive = true
+        } else {
+            fatalError("Não pode fazer o que quer")
+        }
+    }
+}
+
+extension LayoutProperty {
+    func equal(to otherAnchor: Anchor, offsetBy constant: CGFloat = 0) {
+        anchor.constraint(equalTo: otherAnchor,
+                          constant: constant).isActive = true
+    }
+    
+    func greaterThanOrEqual(to otherAnchor: Anchor,
+                            offsetBy constant: CGFloat = 0) {
+        anchor.constraint(greaterThanOrEqualTo: otherAnchor,
+                          constant: constant).isActive = true
+    }
+    
+    func lessThanOrEqual(to otherAnchor: Anchor,
+                         offsetBy constant: CGFloat = 0) {
+        anchor.constraint(lessThanOrEqualTo: otherAnchor,
+                          constant: constant).isActive = true
+    }
+}
+
+
+// Operator overloading
+
+
+func +<A: LayoutAnchor>(lhs: A, rhs: CGFloat) -> (A, CGFloat) {
+    return (lhs, rhs)
+}
+
+
+func -<A: LayoutAnchor>(lhs: A, rhs: CGFloat) -> (A, CGFloat) {
+    return (lhs, -rhs)
+}
+
+func ==<A: LayoutAnchor>(lhs: LayoutProperty<A>, rhs: (A, CGFloat)) {
+    lhs.equal(to: rhs.0, offsetBy: rhs.1)
+}
+
+func ==<A: LayoutAnchor>(lhs: LayoutProperty<A>, rhs: A) {
+    lhs.equal(to: rhs)
+}
+
+func >=<A: LayoutAnchor>(lhs: LayoutProperty<A>,
+                         rhs: (A, CGFloat)) {
+    lhs.greaterThanOrEqual(to: rhs.0, offsetBy: rhs.1)
+}
+
+func >=<A: LayoutAnchor>(lhs: LayoutProperty<A>, rhs: A) {
+    lhs.greaterThanOrEqual(to: rhs)
+}
+
+func <=<A: LayoutAnchor>(lhs: LayoutProperty<A>,
+                         rhs: (A, CGFloat)) {
+    lhs.lessThanOrEqual(to: rhs.0, offsetBy: rhs.1)
+}
+
+func <=<A: LayoutAnchor>(lhs: LayoutProperty<A>, rhs: A) {
+    lhs.lessThanOrEqual(to: rhs)
+}
